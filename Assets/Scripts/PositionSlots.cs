0c3ed05d3cf37e3074dc30efcaf001ed9480fd53
Matej Buday <rplnt@o0.sk>
2017-12-02 23:04:21 +0100

    stuff...

---
+++ Assets/Scripts/PositionSlots.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSlots : MonoBehaviour {
+
+    private void Start() {
+        foreach(Transform child in this.transform) {
+            GameObject bone = GameObject.Find("Bone_" + child.name);
+            if (bone == null) {
+                Debug.Log("Missing bone");
+            }
+
+            child.position = bone.transform.position;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Wobble.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wobble : MonoBehaviour {
+
+    public float verticalWobbleSpeed;
+    public Vector3 wobbleLimits;
+
+
+    private void Update() {
+        transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time, 0.1f / verticalWobbleSpeed) * verticalWobbleSpeed, transform.localPosition.y);
+        transform.localEulerAngles = new Vector3(
+            Mathf.PingPong(Time.time, 2.0f * wobbleLimits.x) - wobbleLimits.x,
+            Mathf.PingPong(Time.time, 2.0f * wobbleLimits.y) - wobbleLimits.y,
+            Mathf.PingPong(Time.time, 2.0f * wobbleLimits.z) - wobbleLimits.z
+            );
+    }
+}
